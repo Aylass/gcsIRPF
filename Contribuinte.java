90dfb3a02bd602137c47fc370a8acabe5ef7c5c5
18280294 <18280294@facin.portoalegre.pucrsnet.br>
2019-09-06 18:31:28 -0300

    criar classe contribuinte

---
+++ Contribuinte.java
@@ -0,0 +1,50 @@
+public class Contribuinte {
+
+    private String nome;
+    private String cpf;
+    private Double baseCalculo;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the baseCalculo
+     */
+    public Double getBaseCalculo() {
+        return baseCalculo;
+    }
+
+    /**
+     * @param baseCalculo the baseCalculo to set
+     */
+    public void setBaseCalculo(Double baseCalculo) {
+        this.baseCalculo = baseCalculo;
+    }
+
+
+}
